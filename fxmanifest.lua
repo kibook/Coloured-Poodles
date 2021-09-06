ee0233542182f5a649133b36e01dbbcffa3a31dc
kibukj <kibuzhj@gmail.com>
2021-09-06 08:10:27 -0400

    Add screenshots, fxmanifest and stream folder

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+authors { 'TayMcKenzieNZ', 'kibukj' }
+description 'Coloured Poodles For FiveM'
+version '2.0.0'
+url 'https://github.com/TayMcKenzieNZ/Coloured-Poodles'
+
+---                                                                     ---
+---   THIS IS A FREE RESOURCE PROVIDED TO CFXE FORUMS FOR FIVEM USAGE   ---
+---        DO NOT REUPLOAD OR MODIFY WITHOUT MY PERMISSION              ---
